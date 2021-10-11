9051721cd10c414099d3e04d505109c7d97f70cb
jarek <szwestek@op.pl>
2021-10-11 12:37:58 +0200

    'graphic created'

---
+++ board.py
@@ -0,0 +1,8 @@
+board = [['greenB', 'greeno', 'black-', 'black-', 'yellowK', 'yellowE', 'yellowH', 'yellowB'],
+         ['greenH', 'greeno', 'black-', 'black-', 'yellowo', 'yellowo', 'yellowo', 'yellowo'],
+         ['greenE', 'greeno', 'black-', 'black-', 'black-', 'black-', 'black-', 'black-'],
+         ['greenK', 'greeno', 'black-', 'black-', 'black-', 'black-', 'black-', 'black-'],
+         ['black-', 'black-', 'black-', 'black-', 'black-', 'black-', 'blueo', 'blueK'],
+         ['black-', 'black-', 'black-', 'black-', 'black-', 'black-', 'blueo', 'blueE'],
+         ['redo', 'redo', 'redo', 'redo', 'black-', 'black-', 'blueo', 'blueH'],
+         ['redB', 'redH', 'redE', 'redK', 'black-', 'black-', 'blueo', 'blueB']]
--- graphic.py
+++ graphic.py
@@ -1,14 +1,39 @@
 from tkinter import *
+from board import *
 root = Tk ()
+root.title ('CZATURANGA')
 
 
-def click ():
-    a.configure(bg='red')
-    text.set('second')
+class BoardButton:
+    def __init__(self, text, color, row, column):
+        self.text = StringVar ()
+        self.text.set (text)
+        self.color = color
+        self.button = Button (root, textvariable=self.text, command=self.click, bg=color)
+        self.button.grid (row=row, column=column)
+
+    def click (self):
+        # self.button.configure(bg='red')
+        # self.text.set('second')
+        pass
 
 
-text = StringVar ()
-text.set('first')
-a = Button (root, textvariable=text, command=click, bg='blue')
-a.grid (row=0, column=0)
+buttons = []
+for i in range (0, 8):
+    for j in range (0, 8):
+        buttons.append (BoardButton (board [i][j][-1], board [i][j][:-1], i, j))
+die1 = Button (root, text='Rzuć kość.')
+die2 = Button (root, text='Rzuć kość')
+action = Button (root, text='Poddaj, wieża, roszada.')
+redPoints = Label (root, text='Punkty twoje / drużyny.', bg='red')
+greenPoints = Label (root, text='Punkty twoje / drużyny.', bg='green')
+yellowPoints = Label (root, text='Punkty twoje / drużyny.', bg='yellow')
+bluePoints = Label (root, text='Punkty twoje / drużyny.', bg='blue')
+die1.grid (row=0, column=8)
+die2.grid (row=1, column=8)
+action.grid (row=2, column=8)
+redPoints.grid (row=3, column=8)
+greenPoints.grid (row=4, column=8)
+yellowPoints.grid (row=5, column=8)
+bluePoints.grid (row=6, column=8)
 root.mainloop()
